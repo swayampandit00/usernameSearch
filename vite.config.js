2e1a12d8f37df5954abe33a29c58068bd6cf47fd
swayampandit00 <swayampandit00@gmail.com>
2026-09-13 02:08:36 +0530

    api add

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3001',
+        target: 'https://usernamesearch-5f00.onrender.com/',
         changeOrigin: true,
       },
     },
